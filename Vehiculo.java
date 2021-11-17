9108b666b03da98075a7d25839c1a2170945fb94
Javier <alvmurjav@gmail.com>
2021-11-17 13:02:31 +0100

    02 El metódo arrancar motor ya no imprime nada.Lo que hace ahora es solo devolver un valor. En el metódo obtenerDetalles se creo una variable local de tipo String y se devuelve esa misma variable con el return

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,4 +1,4 @@
- public class Vehiculo{
+public class Vehiculo{
     private String tractor;
     private int capacidadDeposito;
     private boolean motorApagado;
@@ -7,41 +7,42 @@
         capacidadDeposito = capacidadDelDeposito;
         motorApagado = true;
     }
-    
+
     public String getTractor(){
         return tractor;
     }
-    
+
     public int getcapacidadDeposito(){
         return capacidadDeposito;
     }
-    
+
     public boolean getMotorApagado(){
         return motorApagado;
     }
-    
+
     public void setmodeloTractor(String nuevoModelo){
         this.tractor = nuevoModelo;
     }
-    
+
     public void aumentarAutonomiaVehiculo(int cantidadLitros){
         capacidadDeposito = capacidadDeposito + cantidadLitros;
     }
-    
-    public void estadoVehiculo (){
+
+    public void arrancarMotor(){
         if(motorApagado == true){
-        System.out.println("El motor se encuentra Apagado");
+            motorApagado = false;
         }
         else{
-        System.out.println("El motor se encuentra Encendido");
+            motorApagado = true;
         }
     }
-    
+
     public void imprimirDetalles(){
         System.out.println("Modelo: " + tractor + "Estado depósito gasolina: " + capacidadDeposito+"Apagado: "+ motorApagado);
     }
-    
+
     public String obtenerDetalles(){
-        return "Modelo del tractor: "+ tractor+ "Estado depósito gasolina: " + capacidadDeposito+"Apagado: "+ motorApagado;
+        String detalles = "Modelo del tractor: "+ tractor+ "Estado depósito gasolina: " + capacidadDeposito+"Apagado: "+ motorApagado;
+        return detalles;
     }
 }
